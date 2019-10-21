4866eec4f532ec2b4fd07ac1377a9722e389e5cf
Meagan Utsey <meagan.utsey@gmail.com>
2019-10-21 19:52:44 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+x = 1
+while x < 10
+  puts "#{x} is less than 10"
+  x += 1
+end
 
 def countdown
+x = 0
+while x < 10
+  puts "#{x} SECONDS" is less than 10"
+  x += "HAPPY NEW YEAR" 
 end
